da8ff10835cb6629fec220b38fc92c4e84842e46
vzvenyach <vzvenyach@dccouncil.us>
2013-12-05 22:39:44 -0500

    added links to GPO

--- js/helper.js
+++ js/helper.js
@@ -1,9 +1,10 @@
 function makeTextBox(data) {
 	var s = data.versions;
 	sCount = s.length - 1;
+	var cite = s[sCount].source.stat;
 	var outString = makeButtons(sCount+1);
 	outString += '<h2> Sec. ' + data.section +'. ' + s[sCount].short_title + '.</h2><div id="sectionBox">';
-	outString += '<h3>Source: ' + s[sCount].source.stat + '</h3>';
+	outString += '<h3>Source: <a href="' + makeStatURL(cite) + '" target="blank">' + cite + '</a> (' + s[sCount].source.date + ')</h3>';
 	outString += s[sCount].text + '</div><p/>';
 	return outString;
 }
@@ -16,4 +17,13 @@ function makeButtons(count) {
 		i++;
 	}
 	return outString;
+}
+
+function makeStatURL (cite) {
+	//cite format: 100 Stat. 3228
+	//http://www.gpo.gov/fdsys/search/citation2.result.STATUTE.action?statute.volume=87&statute.pageNumber=795&publication=STATUTE
+
+	var out = cite.match(/\d+/g);
+	url = 'http://www.gpo.gov/fdsys/search/citation2.result.STATUTE.action?statute.volume=' + out[0] + '&statute.pageNumber=' + out[1] +'&publication=STATUTE';
+	return url;
 }
